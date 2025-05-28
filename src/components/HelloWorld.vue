4bb89db559094f9d4d42e455c74a72ac26bb3980
Edwardo928 <edwardoignace928@gmail.com>
2025-05-28 23:19:35 +0300

    Seulement CI pour circle ci

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,8 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ count }}</button>
   </div>
 
 </template>
